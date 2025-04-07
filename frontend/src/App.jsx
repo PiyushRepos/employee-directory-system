22e5c43bb0e7318bdfe972e1936e080ad133a4f9
Piyush kumar <piyush2004.dev@gmail.com>
2025-04-07 14:40:20 +0530

    Initialize frontend project with React, Vite, and Tailwind CSS; add essential files and configurations.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="text-2xl font-bold text-green-300 text-center">
+      Employee Directory System
+    </div>
+  );
+}
+
+export default App;
