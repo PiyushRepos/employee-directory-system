19f293200d81574ae8c7c6aed30d0604822ef705
Piyush kumar <piyush2004.dev@gmail.com>
2025-04-09 01:37:40 +0530

    add cors origin

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,12 @@ import cors from "cors";
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: [process.env.FRONTEND_URL], credentials: true }));
+app.use(
+  cors({
+    origin: [`${process.env.FRONTEND_URL}`, "http://localhost:5173"],
+    credentials: true,
+  })
+);
 
 // index route
 app.get("/", (req, res) => {
