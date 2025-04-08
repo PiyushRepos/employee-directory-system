ca04aa7ed9b0148c0494c9146c92888cb849a31e
Piyush kumar <piyush2004.dev@gmail.com>
2025-04-09 01:27:42 +0530

    change frontend url

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ import cors from "cors";
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(cors({ origin: [process.env.FRONTEND_URL], credentials: true }));
 
 // index route
 app.get("/", (req, res) => {
