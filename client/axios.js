876778392fd0359ebdbc916b8d42974105214df0
Piyush kumar <piyush2004.dev@gmail.com>
2025-04-10 10:51:31 +0530

    fix

--- client/axios.js
+++ client/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
+
+export default axiosInstance;
